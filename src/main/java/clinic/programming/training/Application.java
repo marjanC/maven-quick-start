fa03ffa3e1caa5ff0b15fa98f7d89f1c32de64ae
Marjan Callewaert <marjan.callewaert@tvh.com>
2017-02-04 14:42:41 +0100

    adding changes to pom

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -2,11 +2,17 @@ package clinic.programming.training;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
 
     public Application() {
         System.out.println ("Inside Application");
+    }
+
+    public int countWords(String words){
+        String[] separateWords = StringUtils.split(words, ' ');
+        return (separateWords == null) ? 0 : separateWords.length;
     }
 
     public void greet(){
@@ -24,5 +30,7 @@ public class Application {
         System.out.println ("Starting Application");
         Application app = new Application();
         app.greet();
+        int count = app.countWords("I have four words");
+        System.out.println("Number of words: " + count);
     }
 }
